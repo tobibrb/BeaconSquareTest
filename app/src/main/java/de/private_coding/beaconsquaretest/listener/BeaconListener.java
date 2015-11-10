aac0d622b619c9419ea892b5fa17bcef44e63b65
Tobi@Tobi-PC <tobi.ffo@googlemail.com>
2015-11-10 17:54:20 +0100

    added beacon listener for writing csv file

---
+++ app/src/main/java/de/private_coding/beaconsquaretest/listener/BeaconListener.java
@@ -0,0 +1,66 @@
+package de.private_coding.beaconsquaretest.listener;
+
+import com.onyxbeacon.listeners.OnyxBeaconsListener;
+import com.onyxbeaconservice.IBeacon;
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Bartz, Tobias on 10.11.2015 at 17:17.
+ */
+public class BeaconListener implements OnyxBeaconsListener {
+
+    private static BeaconListener sInstance;
+    public boolean capture;
+
+    public BeaconListener() {
+        super();
+        this.capture = false;
+    }
+
+    public void setCapture(boolean capture) {
+        this.capture = capture;
+    }
+
+    public static BeaconListener getInstance() {
+        if (sInstance == null) {
+            sInstance = new BeaconListener();
+            return sInstance;
+        } else {
+            return sInstance;
+        }
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(List<IBeacon> list) {
+        if (capture) {
+            List<String[]> data = new ArrayList<>();
+            String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+            for (IBeacon beacon : list) {
+                    data.add(new String[] {
+                            String.valueOf(new Date().getTime()),
+                            String.valueOf(beacon.getMajor()),
+                            String.valueOf(beacon.getMinor()),
+                            String.valueOf(beacon.getRssi())
+                    });
+            }
+            try {
+                CSVWriter writer = new CSVWriter(new FileWriter(csv));
+                writer.writeAll(data);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onError(int i, Exception e) {
+
+    }
+}
